6d765829f547a8f09ab3c0a3c743e0bec588907f
Kushal baragi <Kushalbaragi83@gmail.com>
2023-05-08 20:40:11 +0530

    basic style files and components 1st day

--- Downloads/Web development/ToDo/todo-app/src/App.js
+++ Downloads/Web development/ToDo/todo-app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Form from "./form";
+import List from "./List";
 
 function App() {
+  const [value, setValue] = useState([]);
+  function todoHandler(e) {
+    setValue([...value, e]);
+    //  console.log(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <div className="todo-wrapper">
+        <h1>To Do App</h1>
+        <Form todo={todoHandler} />
+        <List task='hey' />
+      {value.map((task, index)=>{
+               <Form todo={todoHandler} />
+
+        {console.log(task);}
+      })}
+       
+      </div>
+    </section>
   );
 }
 
