9eaf9e810c77d409ad9d3353fc19c4f4ba228854
Kushal baragi <Kushalbaragi83@gmail.com>
2023-05-23 19:01:04 +0530

    initial push

--- Downloads/Web development/ToDo/todo-app/src/List.js
+++ Downloads/Web development/ToDo/todo-app/src/List.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 function List(task) {
   return (
-    <div class='task'>{task.task}</div>
+    
+    <table>
+        <thead >
+            <td>number</td>
+            <td>name</td>
+            <td Style= "background-color:white; color:black"> middle name</td>
+            <td>surname</td>
+        </thead>
+        <tr>
+        <td>number</td>
+            <td>name</td>
+            <td>middle name</td>
+            <td>surname</td> 
+        </tr>
+        <tr>
+        <td>number</td>
+            <td>name</td>
+            <td>middle name</td>
+            <td>surname</td> 
+        </tr>
+        <tr>
+        <td>number</td>
+            <td>name</td>
+            <td>middle name</td>
+            <td>surname</td> 
+        </tr>
+    </table>
   )
 }
 
